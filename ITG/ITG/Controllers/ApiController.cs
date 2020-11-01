da2e98cc949c8cc32a882249d09becd956368fbf
MarinaB <m.beloglazenko@ideas-world.com>
2020-11-01 07:57:05 +0300

    added personal account page

--- ITG/ITG/Controllers/ApiController.cs
+++ ITG/ITG/Controllers/ApiController.cs
@@ -11,12 +11,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ITG.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
     public class ApiController : ControllerBase
     {
         public async Task<string> GetApiMenu()
         {
+            //https://etp.gpb.ru/api/procedures.php?inn=7724514910&kpp=772801001&late=10
             string result = "";
             int index = 0;
 
@@ -28,30 +27,21 @@ namespace ITG.Controllers
                 var content = new FormUrlEncodedContent(
                     new Dictionary<string, string>
                     {
+                    { "inn", "7724514910" },
+                    { "kpp", "772801001" },
                     { "late", "1" }
                     });
 
-                var response = await client.PostAsync("api/company_list.php", content);
+                var response = await client.PostAsync("api/procedures.php", content);
                 var xml = new XmlSerializer(typeof(Companies));
 
                 using (var sr = new StringReader(await response.Content.ReadAsStringAsync()))
                 {
-                    var companies = (Companies)xml.Deserialize(sr);
-
-
-
-                    foreach (Company i in companies.company)
-                    {
-                        result += "\nID: " + i.id + "\nINN: " + i.inn + "\nKPP: " + i.kpp + "\nDate Accept: " + i.date_accept + "\n";
-
-                        index++;
-
-                        if (index == 5)
-                        {
-                            break;
-                        }
-                    }
-                    return result;
+                    result = sr.ReadToEnd();
+                    Response.Clear();
+                    Response.ContentType = "text/xml";
+                    var a = Response.WriteAsync(result);
+                    return Convert.ToString(a);
                 }
             }
             catch
@@ -63,9 +53,9 @@ namespace ITG.Controllers
         }
         public async Task<string> GetApiMenu2()
         {
-            string result = "";
-            int index = 0;
+            //https://etp.gpb.ru/api/company_list.php?late=1
 
+            string result = "";
             try
             {
                 HttpClient client = new HttpClient();
@@ -82,35 +72,25 @@ namespace ITG.Controllers
 
                 using (var sr = new StringReader(await response.Content.ReadAsStringAsync()))
                 {
-                    var companies = (Companies)xml.Deserialize(sr);
-
-
-
-                    foreach (Company i in companies.company)
-                    {
-                        result += "\nID: " + i.id + "\nINN: " + i.inn + "\nKPP: " + i.kpp + "\nDate Accept: " + i.date_accept + "\n";
-
-                        index++;
-
-                        if (index == 5)
-                        {
-                            break;
-                        }
-                    }
-                    return result;
+                    result = sr.ReadToEnd();
+                    Response.Clear();
+                    Response.ContentType = "text/xml";
+                    var a = Response.WriteAsync(result);
+                    return Convert.ToString(a);
                 }
             }
             catch
             {
-                return "Error";
+                return "Не удалось подключиться";
             }
 
 
         }
         public async Task<string> GetApiMenu3()
         {
+            //https://etp.gpb.ru/api/company.php?inn=7744001497&kpp=997950001
+
             string result = "";
-            int index = 0;
 
             try
             {
@@ -120,89 +100,63 @@ namespace ITG.Controllers
                 var content = new FormUrlEncodedContent(
                     new Dictionary<string, string>
                     {
-                    { "late", "1" }
+                    { "inn", "7744001497" },
+                    { "kpp", "997950001" }
                     });
 
-                var response = await client.PostAsync("api/company_list.php", content);
+                var response = await client.PostAsync("company.php", content);
                 var xml = new XmlSerializer(typeof(Companies));
 
                 using (var sr = new StringReader(await response.Content.ReadAsStringAsync()))
                 {
-                    var companies = (Companies)xml.Deserialize(sr);
-
-
-
-                    foreach (Company i in companies.company)
-                    {
-                        result += "\nID: " + i.id + "\nINN: " + i.inn + "\nKPP: " + i.kpp + "\nDate Accept: " + i.date_accept + "\n";
-
-                        index++;
-
-                        if (index == 5)
-                        {
-                            break;
-                        }
-                    }
-                    return result;
+                    result = sr.ReadToEnd();
+                    Response.Clear();
+                    Response.ContentType = "text/xml";
+                    var a = Response.WriteAsync(result);
+                    return Convert.ToString(a);
                 }
             }
             catch
             {
-                return "Error";
+                return "Не удалось подключиться";
             }
 
 
         }
         public async Task<string> GetApiMenu4()
         {
+            //https://etp.gpb.ru/api/protocols_list.php?updatedonly
+
             string result = "";
-            int index = 0;
 
             try
             {
                 HttpClient client = new HttpClient();
                 client.BaseAddress = new Uri("https://etp.gpb.ru");
 
-                var content = new FormUrlEncodedContent(
-                    new Dictionary<string, string>
-                    {
-                    { "late", "1" }
-                    });
-
-                var response = await client.PostAsync("api/company_list.php", content);
+                var response = await client.GetAsync("api/protocols_list.php");
                 var xml = new XmlSerializer(typeof(Companies));
 
                 using (var sr = new StringReader(await response.Content.ReadAsStringAsync()))
                 {
-                    var companies = (Companies)xml.Deserialize(sr);
-
-
-
-                    foreach (Company i in companies.company)
-                    {
-                        result += "\nID: " + i.id + "\nINN: " + i.inn + "\nKPP: " + i.kpp + "\nDate Accept: " + i.date_accept + "\n";
-
-                        index++;
-
-                        if (index == 5)
-                        {
-                            break;
-                        }
-                    }
-                    return result;
+                    result = sr.ReadToEnd();
+                    Response.Clear();
+                    Response.ContentType = "text/xml";
+                    var a = Response.WriteAsync(result);
+                    return Convert.ToString(a);
                 }
             }
             catch
             {
-                return "Error";
+                return "Не удалось подключиться";
             }
 
 
         }
         public async Task<string> GetApiMenu5()
         {
+            //https://etp.gpb.ru/api/protocols.php?num=ГП410451
             string result = "";
-            int index = 0;
 
             try
             {
@@ -212,176 +166,26 @@ namespace ITG.Controllers
                 var content = new FormUrlEncodedContent(
                     new Dictionary<string, string>
                     {
-                    { "late", "1" }
+                    { "num", "ГП410451" },
                     });
 
-                var response = await client.PostAsync("api/company_list.php", content);
+                var response = await client.PostAsync("api/protocols.php", content);
                 var xml = new XmlSerializer(typeof(Companies));
 
                 using (var sr = new StringReader(await response.Content.ReadAsStringAsync()))
                 {
-                    var companies = (Companies)xml.Deserialize(sr);
-
-
-
-                    foreach (Company i in companies.company)
-                    {
-                        result += "\nID: " + i.id + "\nINN: " + i.inn + "\nKPP: " + i.kpp + "\nDate Accept: " + i.date_accept + "\n";
-
-                        index++;
-
-                        if (index == 5)
-                        {
-                            break;
-                        }
-                    }
-                    return result;
+                    result = sr.ReadToEnd();
+                    Response.Clear();
+                    Response.ContentType = "text/xml";
+                    var a = Response.WriteAsync(result);
+                    return Convert.ToString(a);
                 }
             }
             catch
             {
-                return "Error";
+                return "Не удалось подключиться";
             }
-
-
         }
-        public async Task<string> GetApiMenu6()
-        {
-            string result = "";
-            int index = 0;
 
-            try
-            {
-                HttpClient client = new HttpClient();
-                client.BaseAddress = new Uri("https://etp.gpb.ru");
-
-                var content = new FormUrlEncodedContent(
-                    new Dictionary<string, string>
-                    {
-                    { "late", "1" }
-                    });
-
-                var response = await client.PostAsync("api/company_list.php", content);
-                var xml = new XmlSerializer(typeof(Companies));
-
-                using (var sr = new StringReader(await response.Content.ReadAsStringAsync()))
-                {
-                    var companies = (Companies)xml.Deserialize(sr);
-
-
-
-                    foreach (Company i in companies.company)
-                    {
-                        result += "\nID: " + i.id + "\nINN: " + i.inn + "\nKPP: " + i.kpp + "\nDate Accept: " + i.date_accept + "\n";
-
-                        index++;
-
-                        if (index == 5)
-                        {
-                            break;
-                        }
-                    }
-                    return result;
-                }
-            }
-            catch
-            {
-                return "Error";
-            }
-
-
-        }
-        public async Task<string> GetApiMenu7()
-        {
-            string result = "";
-            int index = 0;
-
-            try
-            {
-                HttpClient client = new HttpClient();
-                client.BaseAddress = new Uri("https://etp.gpb.ru");
-
-                var content = new FormUrlEncodedContent(
-                    new Dictionary<string, string>
-                    {
-                    { "late", "1" }
-                    });
-
-                var response = await client.PostAsync("api/company_list.php", content);
-                var xml = new XmlSerializer(typeof(Companies));
-
-                using (var sr = new StringReader(await response.Content.ReadAsStringAsync()))
-                {
-                    var companies = (Companies)xml.Deserialize(sr);
-
-
-
-                    foreach (Company i in companies.company)
-                    {
-                        result += "\nID: " + i.id + "\nINN: " + i.inn + "\nKPP: " + i.kpp + "\nDate Accept: " + i.date_accept + "\n";
-
-                        index++;
-
-                        if (index == 5)
-                        {
-                            break;
-                        }
-                    }
-                    return result;
-                }
-            }
-            catch
-            {
-                return "Error";
-            }
-
-
-        }
-        public async Task<string> GetApiMenu8()
-        {
-            string result = "";
-            int index = 0;
-
-            try
-            {
-                HttpClient client = new HttpClient();
-                client.BaseAddress = new Uri("https://etp.gpb.ru");
-
-                var content = new FormUrlEncodedContent(
-                    new Dictionary<string, string>
-                    {
-                    { "late", "1" }
-                    });
-
-                var response = await client.PostAsync("api/company_list.php", content);
-                var xml = new XmlSerializer(typeof(Companies));
-
-                using (var sr = new StringReader(await response.Content.ReadAsStringAsync()))
-                {
-                    var companies = (Companies)xml.Deserialize(sr);
-
-
-
-                    foreach (Company i in companies.company)
-                    {
-                        result += "\nID: " + i.id + "\nINN: " + i.inn + "\nKPP: " + i.kpp + "\nDate Accept: " + i.date_accept + "\n";
-
-                        index++;
-
-                        if (index == 5)
-                        {
-                            break;
-                        }
-                    }
-                    return result;
-                }
-            }
-            catch
-            {
-                return "Error";
-            }
-
-
-        }
     }
 }
--- ITG/ITG/Controllers/HomeController.cs
+++ ITG/ITG/Controllers/HomeController.cs
@@ -32,7 +32,7 @@ namespace ITG.Controllers
         }
         public IActionResult menu()
         {
-            return Ol();
+            return View();
         }
         public IActionResult menu2()
         {
@@ -62,116 +62,20 @@ namespace ITG.Controllers
         {
             return View();
         }
-        public IActionResult Ol()
-        {
-            return View("menu", "menu6");
-        }
-
-        public async Task<string> GetApiMenu()
-        {
-            string result = "";
-            int index = 0;
-
-            try
-            {
-                HttpClient client = new HttpClient();
-                client.BaseAddress = new Uri("https://etp.gpb.ru");
-
-                var content = new FormUrlEncodedContent(
-                    new Dictionary<string, string>
-                    {
-                    { "late", "1" },
-                    { "inn", "7724514910" },
-                    { "cpp", "772801001" }
-                    });
-
-                var response = await client.PostAsync("api/procedures.php", content);
-                var xml = new XmlSerializer(typeof(Procedures));
-
-                result += xml;
-                result += response;
-                result += await response.Content.ReadAsStringAsync();
-
-                using (var sr = new StringReader(await response.Content.ReadAsStringAsync()))
-                {
-                    var procedures = (Procedures)xml.Deserialize(sr);
-
-                    result += procedures;
-
-                    foreach (Procedure i in procedures.procedure)
-                    {
-                        result += "\nИдентификатор: " + i.number + "\nРегистрационный Номер: " + i.registry_number + "\nСтатус: " + i.status + "\n";
-
-                        index++;
-
-                        if (index == 5)
-                        {
-                            break;
-                        }
-                    }
-                    return result;
-                }
-            }
-            catch
-            {
-                return "Error";
-            }
-            
-            
-        }
-
-        public async Task<string> GetApiMenu2()
-        {
-            string result = "";
-            int index = 0;
-
-            try
-            {
-                HttpClient client = new HttpClient();
-                client.BaseAddress = new Uri("https://etp.gpb.ru");
-
-                var content = new FormUrlEncodedContent(
-                    new Dictionary<string, string>
-                    {
-                    { "late", "1" }
-                    });
-
-                var response = await client.PostAsync("api/company_list.php", content);
-                var xml = new XmlSerializer(typeof(Companies));
-
-                using (var sr = new StringReader(await response.Content.ReadAsStringAsync()))
-                {
-                    var companies = (Companies)xml.Deserialize(sr);
-
-
-
-                    foreach (Company i in companies.company)
-                    {
-                        result += "\nID: " + i.id + "\nINN: " + i.inn + "\nKPP: " + i.kpp + "\nDate Accept: " + i.date_accept + "\n";
-
-                        index++;
-
-                        if (index == 5)
-                        {
-                            break;
-                        }
-                    }
-                    return result;
-                }
-            }
-            catch
-            {
-                return "Error";
-            }
-
-
-        }
 
         public IActionResult SandBox()
         {
             return View();
         }
-        
+        public void send_doc(String reason, String name, String month, String day, String year)
+        {
+            //sent doc
+        }
+        public IActionResult PersonalAccount()
+        {
+            return View();
+        }
+
         public string SandBoxGet(string height, string altitude)
         {
             return height+ altitude;
